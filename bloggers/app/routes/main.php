828b28ff26a38fe0fd599c7b7858528f9d21b754
Vladimir Sulatskov <2sulla@gmail.com>
2012-05-14 11:15:56 +0400

    Проверка авторзован ли пользователь на осноном сайте тез клуба

--- bloggers/app/routes/main.php
+++ bloggers/app/routes/main.php
@@ -1,4 +1,18 @@
 <?php
+// проверяем автологин пользователя
+if ( isset($_COOKIE['key']) && $_COOKIE['key'] != ''){
+    // получаем пользователя
+    $session = $db->dbFetchObject("SELECT * FROM tc_session
+                                      WHERE session_key = '" . $_COOKIE['key'] . "'");
+    if ( isset($session) && $session != null  ) {
+        // достаём пользователя
+        $user = $db->dbFetchObject("SELECT * FROM tc_user
+                                      WHERE user_id = '" . $session->user_id . "'");
+        $user->user_profile_avatar = str_replace('100x100', '48x48', $user->user_profile_avatar);
+    }
+    $app->view()->setData( 'userCurrent', $user );
+}
+
 // index page
 $app->get('/', function() use ($app, $db) {
 
