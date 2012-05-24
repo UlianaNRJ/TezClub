9bf807b377dfbb41dcec53564fb414dc03f8c122
Vladimir Sulatskov <2sulla@gmail.com>
2012-05-24 11:33:10 +0400

    Фикс 500 ошибки при входе на сайт, если пользователь был залогинен ранее на осноном. Было в связи  с умиранием куков.

--- bloggers/app/routes/main.php
+++ bloggers/app/routes/main.php
@@ -9,12 +9,13 @@ if ( !empty($_COOKIE['key']) ){
         $user = $db->dbFetchObject("SELECT * FROM tc_user
                                       LEFT JOIN users_ext ON (users_ext.id = tc_user.user_id) 
                                       WHERE tc_user.user_id = '" . $session->user_id . "'");
-        $user->user_profile_avatar = str_replace('100x100', '48x48', $user->user_profile_avatar);
-
-        $app->view()->setData( 'userCurrent', $user );
+        if (!empty($user)) {
+            $user->user_profile_avatar = str_replace('100x100', '48x48', $user->user_profile_avatar);
+            $app->view()->setData( 'userCurrent', $user );
+        }
     }
     // session_security_key
-    if (isset($_COOKIE['tezclub'])) {
+    if (!empty($_COOKIE['tezclub'])) {
         $app->view()->setData('security_ls_key', md5($_COOKIE['tezclub'].'livestreet_security_key'));
     }
 }
