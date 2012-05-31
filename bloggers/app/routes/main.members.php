3e2f04f63a31b0060b50174ee98e5f9cd6c84133
Vladimir Sulatskov <2sulla@gmail.com>
2012-05-31 16:30:29 +0400

    Вывод пользователй - учатсников в конкурсе

--- bloggers/app/routes/main.members.php
+++ bloggers/app/routes/main.members.php
@@ -2,7 +2,17 @@
 
 $app->get('/members', function() use ($app, $db) {
 
-    $data = array('currentpage' => 'members');
+    $bloggers = $db->dbFetchAll("SELECT * 
+                                    FROM tc_user
+                                    LEFT JOIN users_ext ON (users_ext.id = tc_user.user_id) 
+                                    WHERE users_ext.participate = '1'
+                                    ORDER BY tc_user.user_rating DESC 
+                                    LIMIT 20
+                                  ");
+
+    $data = array('currentpage' => 'members',
+                  'bloggers'    => $bloggers
+                  );
 
     return $app->render('front/members.twig', $data);
 });
