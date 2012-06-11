412001946427df9e3e4d7985bcf313ea1c84cad2
Vladimir Sulatskov <2sulla@gmail.com>
2012-06-11 14:07:49 +0400

    Вывод финалситов 2

--- bloggers/app/routes/main.members.php
+++ bloggers/app/routes/main.members.php
@@ -2,6 +2,12 @@
 
 $app->get('/members', function() use ($app, $db) {
 
+    $finalists = $db->dbFetchAll("SELECT bbfin.*, tc_user.user_login, tc_user.user_profile_name
+                                    FROM bb_fin_users as bbfin
+                                    LEFT JOIN tc_user ON (bbfin.user_id = tc_user.user_id) 
+                                    ORDER BY bbfin.mounth ASC
+                                  ");
+
     $bloggers = $db->dbFetchAll("SELECT * 
                                     FROM tc_user
                                     LEFT JOIN users_ext ON (users_ext.id = tc_user.user_id) 
@@ -11,7 +17,8 @@ $app->get('/members', function() use ($app, $db) {
                                   ");
 
     $data = array('currentpage' => 'members',
-                  'bloggers'    => $bloggers
+                  'bloggers'    => $bloggers,
+                  'finalists'    => $finalists
                   );
 
     return $app->render('front/members.twig', $data);
